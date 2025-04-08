d14cb0a23ac1593ee2c259bbc93065f129d5221e
Jobayer Mojumder <jobayer.mojumder@burda-forward.de>
2025-04-08 11:38:58 +0200

    code updated

--- feature-engineering/1_dt_main.py
+++ feature-engineering/1_dt_main.py
@@ -67,6 +67,6 @@ def run_all_single_feature():
 
 
 if __name__ == "__main__":
-    # run_combinations()
-    run_all_single_feature()
-    run_all_general_combinations()
+    run_combinations()
+    # run_all_single_feature()
+    # run_all_general_combinations()
